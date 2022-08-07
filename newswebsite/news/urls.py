f49fce537e84817d981ac0e33663b42a33210375
Сергей Кастов <smailix1@gmail.com>
2022-08-07 17:24:10 +0300

    comments, urls

---
+++ newswebsite/news/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from news import views
+from .views import index, create_news, post_detail, add_comment, DeleteComment, LikeView
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('create/', create_news, name='create_news'),
+    path('<int:post_id>/', post_detail, name='post_detail'),
+    path('edit/<int:id>/', views.edit, name='post_edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('<int:id>/comment/', add_comment, name='add_comment'),
+    path('dele/<int:id>/', DeleteComment, name='del'),
+    path('like/<int:pk>', LikeView, name='like_post'),
+]
--- newswebsite/newswebsite/urls.py
+++ newswebsite/newswebsite/urls.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from news import views
-from news.views import index, create_news, post_detail, add_comment, DeleteComment, LikeView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('create/', create_news, name='create_news'),
-    path('<int:post_id>/', post_detail, name='post_detail'),
-    path('edit/<int:id>/', views.edit, name='post_edit'),
-    path('delete/<int:id>/', views.delete, name='delete'),
-    path('<int:id>/comment/', add_comment, name='add_comment'),
-    path('dele/<int:id>/', DeleteComment, name='del'),
-    path('like/<int:pk>', LikeView, name='like_post'),
+    path('', include('news.urls')),
 ]
